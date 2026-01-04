ede53eca6955a6d4cb7e7b73a6cf074a2e0c10b6
stiernacken <stierzudemnacken@gmail.com>
2026-01-04 22:08:48 +0100

    everythin

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,36 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("life-progress").then(cache =>
-      cache.addAll(["./", "./index.html"])
+const CACHE_NAME = "app-cache-v1";
+const urlsToCache = [
+  "./index.html",
+  "./bipolar.ttf",
+  "./background.jpg",
+  "./favicon.png"
+];
+
+// Install - caching files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate - cleanup old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
     )
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+// Fetch - serve cached files if offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
